2f4fb052340aa6e5103f98497abc9cf69ea451f9
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-02-02 13:13:44 +0500

    home work

---
+++ index.js
@@ -0,0 +1,57 @@
+// 1-masal
+
+// let arr = [2, 45, 34, 10, 24, 6];
+// function tenMax(arr) {
+//   let newNum = arr.filter((value) => {
+//     return value >= 10;
+//   });
+//   return newNum
+// }
+// console.log(tenMax(arr));
+
+// 2-masal
+
+// let arr = [3, 5, 4, 7, 6];
+
+// function kube(arr) {
+//   let newAre = arr.map((value) => {
+//     return value ** 3;
+//   });
+//   return newAre;
+// }
+// console.log(kube(arr));
+
+// 3-masal
+
+// let arr = [14, 35, 39, 45, 20];
+// function divisor(arr) {
+//   let res = ` 3ga va 5ga karralik emas`;
+//   let bool = arr.some((value) => {
+//     if (value % 3 == 0 && value % 5 == 0) {
+//       res = `3ga va 5ga karralik`;
+//     }
+//   });
+
+//   return res;
+// }
+// console.log(divisor(arr));
+
+// 4-masal
+
+// let arr = [5, 10, 8, 15, 9, 27, 55];
+// function big(arr) {
+//   let storage = arr.sort((a, b) => b - a);
+//   return storage.splice(2, 1);
+// }
+// console.log(big(arr));
+
+// 5-masal
+
+// let arr = [5, 4, 3, 2, 1];
+// function sorting(arr) {
+//   let order = arr.reverse().map((v) => {
+//     return v.toString();
+//   });
+//   return order
+// }
+// console.log(sorting(arr));
